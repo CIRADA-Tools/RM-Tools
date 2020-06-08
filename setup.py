97ffc5b2c211fdb08522808e0686e073b7649b5c
Cameron Van Eck <cameron.van.eck@dunlap.utoronto.ca>
2020-06-08 09:11:47 -0400

    Updated version number for patch (forgot to do so before).

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ NAME = 'RM-Tools'
 DESCRIPTION = 'RM-synthesis, RM-clean and QU-fitting on polarised radio spectra'
 URL = 'https://github.com/CIRADA-Tools/RM-Tools'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DOWNLOAD_URL = 'https://github.com/CIRADA-Tools/RM-Tools/archive/v1.0.0.tar.gz'
 
 REQUIRED = [
