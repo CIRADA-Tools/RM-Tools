492a798c15ec20df4b9d2e82333024f00e8f2e9a
Thomson, Alec (S&A, Kensington) <alec.thomson@csiro.au>
2022-02-09 17:19:39 +0800

    Fix arg parsing and add plotting

--- RMtools_1D/clean_model.py
+++ RMtools_1D/clean_model.py
@@ -10,8 +10,13 @@ Author: cvaneck, Aug 2021
 
 import numpy as np
 from RMtools_1D.do_RMsynth_1D import readFile as read_freqFile
+from RMutils.util_misc import create_frac_spectra
+from RMutils.util_misc import calculate_StokesI_model
+from RMutils.util_misc import toscalar
+from RMutils.util_plotTk import plot_Ipqu_spectra_fig
 from RMutils.util_misc import calculate_StokesI_model
 import json
+import matplotlib.pyplot as plt
 
 def calculate_QU_model(freqArr, phiArr, CCArr, lambdaSq_0, Iparms=None):
     """Compute the predicted Stokes Q and U values for each channel from a
@@ -72,7 +77,6 @@ def read_files(freqfile,rmSynthfile, CCfile):
             Iparms: list of Stokes I model parameters.
             lambdaSq_0: scalar value of lambda^2_0, in m^2.
     """
-    
     phiArr, CCreal, CCimag = np.loadtxt(CCfile, unpack=True, dtype='float')
     CCArr=CCreal + 1j * CCimag
     
@@ -81,12 +85,61 @@ def read_files(freqfile,rmSynthfile, CCfile):
     Iparms=[float(x) for x in synth_mDict['polyCoeffs'].split(',')]
     lambdaSq_0=synth_mDict['lam0Sq_m2']
     
+    # Parse the data array
+    # freq_Hz, I, Q, U, dI, dQ, dU
     data=read_freqFile(freqfile, 64, verbose=False, debug=False)
-    freqArr=data[0]
-    
+    freqArr_Hz=data[0]
 
-    return phiArr, CCArr, Iparms, lambdaSq_0,freqArr
+    return phiArr, CCArr, Iparms, lambdaSq_0,freqArr_Hz
 
+
+def plot_model(freqfile, QUarr,Imodel):
+    # Parse the data array
+    # freq_Hz, I, Q, U, dI, dQ, dU
+    data=read_freqFile(freqfile, 64, verbose=False, debug=False)
+    try:
+        (freqArr_Hz, IArr, QArr, UArr,
+         dIArr, dQArr, dUArr) = data
+        print("\nFormat [freq_Hz, I, Q, U, dI, dQ, dU]")
+    except Exception:
+        # freq_Hz, Q, U, dQ, dU
+        try:
+            (freqArr_Hz, QArr, UArr, dQArr, dUArr) = data
+
+            print("\nFormat [freq_Hz, Q, U,  dQ, dU]")
+            noStokesI = True
+        except Exception:
+            print("\nError: Failed to parse data file!")
+    dataArr = create_frac_spectra(freqArr=freqArr_Hz,
+                                    IArr=IArr,
+                                    QArr=QArr,
+                                    UArr=UArr,
+                                    dIArr=dIArr,
+                                    dQArr=dQArr,
+                                    dUArr=dUArr,
+                                    polyOrd=2,
+                                    verbose=True,
+                                    fit_function='log')
+    (IModArr, qArr, uArr, dqArr, duArr, IfitDict) = dataArr
+
+    freqHirArr_Hz =  np.linspace(freqArr_Hz[0], freqArr_Hz[-1], 10000)
+
+    IModHirArr = calculate_StokesI_model(IfitDict, freqHirArr_Hz)
+
+    specFig = plt.figure(facecolor='w',figsize=(10, 6))
+    plot_Ipqu_spectra_fig(freqArr_Hz     = freqArr_Hz,
+                            IArr           = IArr,
+                            qArr           = qArr,
+                            uArr           = uArr,
+                            dIArr          = dIArr,
+                            dqArr          = dqArr,
+                            duArr          = duArr,
+                            freqHirArr_Hz  = freqArr_Hz,
+                            IModArr        = Imodel,
+                            qModArr        = QUarr.real,
+                            uModArr        = QUarr.imag,
+                            fig            = specFig)
+    plt.show()
 
 def main():
     """Generate Stokes QU model based on clean components and (optional) 
@@ -104,13 +157,13 @@ def main():
     # Parse the command line options
     parser = argparse.ArgumentParser(description=descStr,
                                  formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("freqfile", metavar="input.dat", nargs=1,
+    parser.add_argument("freqfile", metavar="input.dat",
                         help="ASCII file containing original frequency spectra.")
-    parser.add_argument("rmSynthfile",metavar="_RMsynth.json",nargs=1,
+    parser.add_argument("rmSynthfile",metavar="_RMsynth.json",
                         help="RMsynth1d output JSON file.")
-    parser.add_argument("CCfile",metavar="_FDFmodel.dat",nargs=1,
+    parser.add_argument("CCfile",metavar="_FDFmodel.dat",
                         help="Clean component model file (_FDFmodel.dat)")
-    parser.add_argument("outfile",metavar="QUmodel.dat",nargs=1,
+    parser.add_argument("outfile",metavar="QUmodel.dat",
                         help="Filename to save output model to.")
     args = parser.parse_args()
 
@@ -119,6 +172,8 @@ def main():
                                                          args.rmSynthfile,
                                                          args.CCfile)
     QUarr,Imodel=calculate_QU_model(freqArr, phiArr, CCArr, lambdaSq_0, Iparms)
+
+    plot_model(args.freqfile, QUarr,Imodel)
     
     save_model(args.outfile,freqArr,Imodel,QUarr)
 
