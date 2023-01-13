169b003c5b5582c795f7ad70783ef42bbd636c59
Thomson, Alec (S&A, Kensington) <AlecThomson@users.noreply.github.com>
2023-01-13 12:15:54 +0800

    Update niter in clean test

--- tests/QA_tests.py
+++ tests/QA_tests.py
@@ -52,7 +52,7 @@ def create_1D_data(freq_arr):
     # noise_spectrum_Q=rng.normal(scale=noise_amplitude,size=freq_arr.shape)
     # noise_spectrum_U=rng.normal(scale=noise_amplitude,size=freq_arr.shape)
     # dIQU=np.ones_like(freq_arr)*noise_amplitude*error_estimate
-    
+
     if not os.path.isdir('simdata/1D'):
         os.makedirs('simdata/1D')
     shutil.copy('RMsynth1D_testdata.dat','simdata/1D/simsource.dat')
@@ -69,7 +69,7 @@ def create_1D_data(freq_arr):
         f.write('Spectral index = {}\n'.format(spectral_index))
         f.write('Actual error per channel = {} Jy/beam\n'.format(noise_amplitude))
         f.write('Input assumed error = {} Jy/beam\n'.format(noise_amplitude*error_estimate))
-        
+
 
 def create_3D_data(freq_arr,N_side=100):
     src_RM=200
@@ -77,14 +77,14 @@ def create_3D_data(freq_arr,N_side=100):
     src_flux=2
     src_x=N_side//4
     src_y=N_side//4
-    
+
     diffuse_RM=50
     diffuse_pol_angle_deg=-10
     diffuse_flux=1
 
     noise_amplitude=0.1
     beam_size_pix=20
-    
+
     src_pol_spectrum=Faraday_thin_complex_polarization(freq_arr,src_RM,src_flux,src_pol_angle_deg)
     diffuse_pol_spectrum=Faraday_thin_complex_polarization(freq_arr,diffuse_RM,diffuse_flux,diffuse_pol_angle_deg)
 
@@ -98,10 +98,10 @@ def create_3D_data(freq_arr,N_side=100):
     scale_factor=np.max(np.sqrt(src_Q_cube**2+src_U_cube**2))/src_flux #Renormalizing flux after convolution
     src_Q_cube=src_Q_cube/scale_factor
     src_U_cube=src_U_cube/scale_factor
-    
+
     diffuse_Q_cube=np.tile(diffuse_pol_spectrum.real[np.newaxis,np.newaxis,:],(N_side,N_side,1))
     diffuse_U_cube=np.tile(diffuse_pol_spectrum.imag[np.newaxis,np.newaxis,:],(N_side,N_side,1))
-    
+
     rng=np.random.default_rng(20200422)
     noise_Q_cube=rng.normal(scale=noise_amplitude,size=src_Q_cube.shape)
     noise_U_cube=rng.normal(scale=noise_amplitude,size=src_Q_cube.shape)
@@ -125,21 +125,21 @@ def create_3D_data(freq_arr,N_side=100):
     header['CDELT1']=-1./3600.
     header['CRPIX1']=1
     header['CUNIT1']='deg'
-    
+
     header['CTYPE2']='DEC--SIN'
     header['CRVAL2']=0
     header['CDELT2']=1./3600.
     header['CRPIX2']=1
     header['CUNIT2']='deg'
-    
+
     header['CTYPE3']='FREQ'
     header['CRVAL3']=freq_arr[0]
     header['CDELT3']=freq_arr[1]-freq_arr[0]
     header['CRPIX3']=1
     header['CUNIT3']='Hz'
-    
+
     header['BUNIT']='Jy/beam'
-    
+
     if not os.path.isdir('simdata/3D'):
         os.makedirs('simdata/3D')
 
@@ -166,7 +166,7 @@ def create_3D_data(freq_arr,N_side=100):
         f.write('Actual error per channel = {} Jy/beam\n'.format(noise_amplitude))
         f.write('Beam FWHM = {} pix\n'.format(beam_size_pix))
 
-        
+
 
 
 class test_RMtools(unittest.TestCase):
@@ -188,7 +188,7 @@ class test_RMtools(unittest.TestCase):
             if type(mDict[key])==str or refDict[key] == 0:
                 self.assertEqual(mDict[key],refDict[key],'{} differs from expectation.'.format(key))
             else:
-                self.assertTrue(np.abs((mDict[key]-refDict[key])/refDict[key]) < 1e-3, 
+                self.assertTrue(np.abs((mDict[key]-refDict[key])/refDict[key]) < 1e-3,
                             '{} differs from expectation.'.format(key))
 
 
@@ -201,11 +201,11 @@ class test_RMtools(unittest.TestCase):
         self.assertEqual(header['NAXIS'],3,'Wrong number of axes in output?')
         self.assertEqual((header['NAXIS1'],header['NAXIS2']),(100,100),'Image plane has wrong dimensions!')
         self.assertEqual(header['NAXIS3'],61,'Number of output FD planes has changed.')
-    
+
     def test_b1_1D_clean(self):
         if not os.path.exists('simdata/1D/simsource_RMsynth.dat'):
             self.skipTest('Could not test 1D clean; 1D synth failed first.')
-        returncode=subprocess.call('rmclean1d simdata/1D/simsource.dat -n 10 -S',shell=True)
+        returncode=subprocess.call('rmclean1d simdata/1D/simsource.dat -n 11 -S',shell=True)
         self.assertEqual(returncode, 0, 'RMclean1D failed to run.')
 
     def test_b2_1D_clean_values(self):
@@ -216,29 +216,29 @@ class test_RMtools(unittest.TestCase):
                             '{} differs from expectation.'.format(key))
 
 
-        
+
     def test_d_3D_clean(self):
         if not os.path.exists('simdata/3D/FDF_tot_dirty.fits'):
             self.skipTest('Could not test 3D clean; 3D synth failed first.')
         returncode=subprocess.call('rmclean3d simdata/3D/FDF_tot_dirty.fits simdata/3D/RMSF_tot.fits -n 10',shell=True)
         self.assertEqual(returncode, 0, 'RMclean3D failed to run.')
         #what else?
-    
+
     def test_e_1Dsynth_fromFITS(self):
         if not os.path.exists('simdata/3D/Q_cube.fits'):
             create_3D_data(self.freq_arr)
         returncode=subprocess.call('rmsynth1dFITS simdata/3D/Q_cube.fits simdata/3D/U_cube.fits 25 25 -l 600 -d 3 -S',shell=True)
         self.assertEqual(returncode, 0, 'RMsynth1D_fromFITS failed to run.')
-        
+
     def test_f1_QUfitting(self):
         if not os.path.exists('simdata/1D/simsource.dat'):
             create_1D_data(self.freq_arr)
-        if not os.path.exists('models_ns'):            
+        if not os.path.exists('models_ns'):
             shutil.copytree('../RMtools_1D/models_ns','models_ns')
         returncode=subprocess.call('qufit simdata/1D/simsource.dat --sampler nestle',shell=True)
         self.assertEqual(returncode, 0, 'QU fitting failed to run.')
         shutil.rmtree('models_ns')
-        
+
     def test_f2_QUfit_values(self):
         mDict = json.load(open('simdata/1D/simsource_m1_nestle.json', "r"))
         #The QU-fitting code has internal randomness that I can't control. So every run
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     unittest.TestLoader.sortTestMethodsUsing=None
     suite = unittest.TestLoader().loadTestsFromTestCase(test_RMtools)
     unittest.TextTestRunner(verbosity=2).run(suite)
-    
+
 
 
 
