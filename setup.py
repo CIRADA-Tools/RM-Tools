6fc0d94d92e2d0e04fd8af2779f85e532bf000d1
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-10-22 16:32:04 +0800

    Protect numpy

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ VERSION = '1.3.1'
 DOWNLOAD_URL = 'https://github.com/CIRADA-Tools/RM-Tools/archive/v'+VERSION+'.tar.gz'
 
 REQUIRED = [
-    'numpy', 'scipy', 'matplotlib>=3.4.0', 'astropy','tdqm', 'deprecation'
+    'numpy<2', 'scipy', 'matplotlib>=3.4.0', 'astropy','tdqm', 'deprecation'
 ]
 
 # Using AT's fork for now - includes tiny bug fix for bilby
