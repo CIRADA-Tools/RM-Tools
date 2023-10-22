18cc1f9dd56c412a8c818925360c778cad066149
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-10-22 16:23:08 +0800

    Add versions

--- RMutils/__init__.py
+++ RMutils/__init__.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 """Dependencies for RM utilities """
+import pkg_resources
 __all__ = ['mpfit',
            'normalize',
            'util_FITS',
@@ -8,3 +9,5 @@ __all__ = ['mpfit',
            'util_plotTk',
            'util_rec',
            'util_RM']
+
+__version__ = pkg_resources.get_distribution("RM-Tools").version
