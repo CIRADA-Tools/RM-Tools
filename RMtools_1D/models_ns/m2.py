4363d975bd6b5be85220af9ca0ef71eb3cd24da0
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-11-27 16:55:52 +0800

    M2

--- RMtools_1D/models_ns/m2.py
+++ RMtools_1D/models_ns/m2.py
@@ -1,39 +1,40 @@
-# =============================================================================#
+#=============================================================================#
 #                          MODEL DEFINITION FILE                              #
-# =============================================================================#
-import bilby
+#=============================================================================#
 import numpy as np
+import bilby
 
-
-# -----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 # Function defining the model.                                                #
 #                                                                             #
 #  pDict       = Dictionary of parameters, created by parsing inParms, below. #
 #  lamSqArr_m2 = Array of lambda-squared values                               #
 #  quArr       = Complex array containing the Re and Im spectra.              #
-# -----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 def model(pDict, lamSqArr_m2):
     """Single Faraday component with Burn depolarisation"""
-
+    
     # Calculate the complex fractional q and u spectra
     pArr = pDict["fracPol"] * np.ones_like(lamSqArr_m2)
-    quArr = (
-        pArr
-        * np.exp(2j * (np.radians(pDict["psi0_deg"]) + pDict["RM_radm2"] * lamSqArr_m2))
-        * np.exp(-2.0 * pDict["sigmaRM_radm2"] ** 2.0 * lamSqArr_m2**2.0)
-    )
-
+    quArr = (pArr * np.exp( 2j * (np.radians(pDict["psi0_deg"]) +
+                                  pDict["RM_radm2"] * lamSqArr_m2))
+             * np.exp(-2.0 * pDict["sigmaRM_radm2"]**2.0 
+                      * lamSqArr_m2**2.0))
+    
     return quArr
 
 
-# -----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 # Priors for the above model.                                                 #
 # See https://lscsoft.docs.ligo.org/bilby/prior.html for details.             #
 #                                                                             #
-# -----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 priors = {
     "fracPol": bilby.prior.Uniform(
-        minimum=0.001, maximum=1.0, name="fracPol", latex_label="$p$"
+        minimum=0.001, 
+        maximum=1.0, 
+        name="fracPol", 
+        latex_label="$p$"
     ),
     "psi0_deg": bilby.prior.Uniform(
         minimum=0,
